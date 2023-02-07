17e7fc783087af84655cbd074a5de019fd5291d7
RiKi <phoenixarmand.ani@georgebrown.ca>
2023-02-06 23:22:41 -0500

    testing mongo

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
+
+// const mongoose = require('mongoose');
+// const Msg = require('./models/postSchema');
+
 const io = require ('socket.io')(3000, {
     cors:{
         origin: ['http://localhost:8080']
     }
 })
-
+// const mongoDb = 'mongodb+srv://phoenixarmand-ani:1234567ani@cluster0.mrvq3t5.mongodb.net/message-app?retryWrites=true&w=majority';
+// mongoose.connect(mongoDb).then(()=>{
+//     console.log('Connected to Database')
+// }).catch(err => console.log(err))
 io.on('connection', socket => {
     console.log(socket.id)
     socket.on('send-message', (message, room) => {
