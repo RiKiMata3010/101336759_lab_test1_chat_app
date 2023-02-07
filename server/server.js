f4853bcd47d5de04a76cad6f19b8100e8bb418f4
RiKi <phoenixarmand.ani@georgebrown.ca>
2023-02-07 09:59:44 -0500

    Finished Monggodb implementation

--- server/server.js
+++ server/server.js
@@ -1,24 +1,29 @@
 
-// const mongoose = require('mongoose');
-// const Msg = require('./models/postSchema');
-
-const io = require ('socket.io')(3000, {
+const mongoose = require('mongoose');
+const io = require ('socket.io')(3000 , {
     cors:{
         origin: ['http://localhost:8080']
     }
 })
-// const mongoDb = 'mongodb+srv://phoenixarmand-ani:1234567ani@cluster0.mrvq3t5.mongodb.net/message-app?retryWrites=true&w=majority';
-// mongoose.connect(mongoDb).then(()=>{
-//     console.log('Connected to Database')
-// }).catch(err => console.log(err))
+const Msg = require('./models/postSchema.js');
+
+const mongoDb = 'mongodb+srv://phoenixarmand-ani:superidol123@cluster0.mrvq3t5.mongodb.net/message-app?retryWrites=true&w=majority';
+mongoose.connect(mongoDb, {useNewUrlParser : true, useUnifiedTopology : true}).then(()=>{
+    console.log('Connected to Database')
+}).catch(err => console.log(err))
+
+
 io.on('connection', socket => {
     console.log(socket.id)
     socket.on('send-message', (message, room) => {
-        if (room === ""){
-           socket.broadcast.emit('receive-message', `${socket.id} : ${message}`)
-        }else {
-           socket.to(room).emit('receive-message', `${socket.id} : ${message}`)
-        }
+        const mail = new Msg({message});
+        mail.save().then(()=>{
+            if (room === ""){
+                socket.broadcast.emit('receive-message', `${socket.id} : ${message}`)
+             }else {
+                socket.to(room).emit('receive-message', `${socket.id} : ${message}`)
+             }
+        })
     })
     socket.on('join-room', (room, status) => {
         socket.join(room)
